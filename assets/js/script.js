ab69c9d321c0e73f2ee67792a88a815a93c504ce
microvac23 <Ithanmachuca@gmail.com>
2023-05-11 23:15:00 -0500

    4/7 Acceptance Criteria Met

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,35 +7,42 @@ var temp = $('#temp')
 var wind = $('#wind')
 var humidity = $('#humidity')
 
-function runCity() { 
-var weatherLoc = `http://api.openweathermap.org/geo/1.0/direct?q=${localStorage.getItem("Search-Entry")}&limit=5&appid=6f64034fc295dc68d6c827ea1f2dc4d8`
- 
-fetch(weatherLoc)
-    .then(response => {
-        return response.json()
-    })
-    .then(data => {
-        console.log(data)
-        localStorage.setItem("Search-Lat", (data[0].lat).toFixed(2))
-        localStorage.setItem("Search-Lon", (data[0].lon).toFixed(2))
-    })
-}
+document.addEventListener("DOMContentLoaded", function(event){
 
 function runWeather() { 
 var weatherApi = `https://api.openweathermap.org/data/2.5/forecast?lat=${localStorage.getItem("Search-Lat")}&lon=${localStorage.getItem("Search-Lon")}&appid=6f64034fc295dc68d6c827ea1f2dc4d8`
- 
+var currentApi = `https://api.openweathermap.org/data/2.5/weather?lat=${localStorage.getItem("Search-Lat")}&lon=${localStorage.getItem("Search-Lon")}&appid=6f64034fc295dc68d6c827ea1f2dc4d8`
+
+fetch(currentApi)
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+            console.log(data)
+            localStorage.setItem("cityName", data.name)
+            localStorage.setItem("currentDate", (dayjs.unix(data.dt).format('YYYY-MM-DD')))
+            localStorage.setItem("currentTemp", ((data.main.feels_like - 273.15) * 9/5 + 32).toFixed(2))
+            localStorage.setItem("currentHumidity", data.main.humidity)
+            localStorage.setItem("currentDescr", data.weather[0].description)
+            localStorage.setItem("currentWind", data.wind.speed)
+            localStorage.setItem("currentIcon", data.weather[0].icon)
+        })
+
+
 fetch(weatherApi)
         .then(response => {
             return response.json()
         })
         .then(data => {
             console.log(data)
-            localStorage.setItem("Search-Date", data.list[0].dt_txt)
-            localStorage.setItem("Search-Temp", ((data.list[0].main.feels_like - 273.15) * 9/5 + 32).toFixed(2))
-            localStorage.setItem("Humidity", data.list[0].main.humidity)
-            localStorage.setItem("Search-Descr", data.list[0].weather[0].description)
-            localStorage.setItem("Wind", data.list[0].wind.speed)
-            localStorage.setItem("Search-Icon", data.list[0].weather[0].icon)
+/*          console.log([ddata.list[0]}, {data.list[6]}, {data.list[14]}, {data.list[22]}, {data.list[30]}]);
+ */         localStorage.setItem("dayList", ({D1: data.list[0], D2: data.list[6], D3: data.list[14], D4: data.list[22], D5: data.list[30]}))
+            localStorage.setItem("futureDate", (dayjs(data.list[0].dt_txt).format('YYYY-MM-DD')))
+            localStorage.setItem("futureTemp", ((data.list[0].main.feels_like - 273.15) * 9/5 + 32).toFixed(2))
+            localStorage.setItem("futureHumidity", data.list[0].main.humidity)
+            localStorage.setItem("futureDescr", data.list[0].weather[0].description)
+            localStorage.setItem("futureWind", data.list[0].wind.speed)
+            localStorage.setItem("futureIcon", data.list[0].weather[0].icon)
         })
     
 }
@@ -44,23 +51,72 @@ searchCity.on("keyup", function(event) {
     if(event.keyCode === 13) {
         event.preventDefault()
         
-        var searchHistory = searchCity.val();
-        localStorage.setItem("Search-Entry", searchHistory)
+        var searchHistoryAp = searchCity.val();
+        localStorage.setItem("Search-Entry", searchHistoryAp)
 
-        var historyEL = $(`<button class="col-12"> ${localStorage.getItem("Search-Entry")} </button>`)
+        var historyEL = $(`<button class="col-12 searchHistory"> ${localStorage.getItem("Search-Entry")} </button>`)
         searchLog.append(historyEL)
+
+        function runCity() { 
+            var weatherLoc = `http://api.openweathermap.org/geo/1.0/direct?q=${localStorage.getItem("Search-Entry")}&limit=5&appid=6f64034fc295dc68d6c827ea1f2dc4d8`
+             
+            fetch(weatherLoc)
+                .then(response => {
+                    return response.json()
+                })
+                .then(data => {
+                    console.log(data)
+                    localStorage.setItem("Search-Lat", (data[0].lat).toFixed(2))
+                    localStorage.setItem("Search-Lon", (data[0].lon).toFixed(2))
+                })
+            }
 
         runCity()
         runWeather()
 
-        searchResult.text(`${localStorage.getItem("Search-Entry")} ${localStorage.getItem("Search-Date")}`)
-        description.text(`${localStorage.getItem("Search-Descr")} `)
-        icon.attr("src", `http://openweathermap.org/img/w/${localStorage.getItem("Search-Icon")}.png`)
-        temp.text(`Temperature: ${localStorage.getItem("Search-Temp")}°F`)    
-        humidity.text(`Humidity: ${localStorage.getItem("Humidity")}%`)  
-        wind.text(`Wind Speed: ${localStorage.getItem("Wind")} mph`)  
+        searchResult.text(`${localStorage.getItem("cityName")} ${localStorage.getItem("currentDate")}`)
+        description.text(`${localStorage.getItem("currentDescr")} `)
+        icon.attr("src", `http://openweathermap.org/img/w/${localStorage.getItem("currentIcon")}.png`)
+        temp.text(`Temperature: ${localStorage.getItem("currentTemp")}°F`)    
+        humidity.text(`Humidity: ${localStorage.getItem("currentHumidity")}%`)  
+        wind.text(`Wind Speed: ${localStorage.getItem("currentWind")} mph`)  
     }
     event.stopPropagation()
 })
-    
-    
+
+var searchHistoryEl = $('.searchHistory')
+$('#searchLog').on("click", searchHistoryEl, function(event){
+    console.log("hello");
+    var searchHistory = event.target.textContent;
+    console.log(searchHistory);
+        localStorage.setItem("recallEntry", searchHistory)
+
+        function recallCity() { 
+            var weatherLoc = `http://api.openweathermap.org/geo/1.0/direct?q=${localStorage.getItem("recallEntry")}&limit=5&appid=6f64034fc295dc68d6c827ea1f2dc4d8`
+             
+            fetch(weatherLoc)
+                .then(response => {
+                    return response.json()
+                })
+                .then(data => {
+                    console.log(data)
+                    localStorage.setItem("Search-Lat", (data[0].lat).toFixed(2))
+                    localStorage.setItem("Search-Lon", (data[0].lon).toFixed(2))
+                })
+            }
+
+        recallCity()
+        runWeather()
+
+        searchResult.text(`${localStorage.getItem("cityName")} ${localStorage.getItem("currentDate")}`)
+        description.text(`${localStorage.getItem("currentDescr")} `)
+        icon.attr("src", `http://openweathermap.org/img/w/${localStorage.getItem("currentIcon")}.png`)
+        temp.text(`Temperature: ${localStorage.getItem("currentTemp")}°F`)    
+        humidity.text(`Humidity: ${localStorage.getItem("currentHumidity")}%`)  
+        wind.text(`Wind Speed: ${localStorage.getItem("currentWind")} mph`)
+
+        
+  });
+
+console.log("done");
+})
